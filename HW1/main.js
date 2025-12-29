577e921688bd92b569ac3e264a91699932489883
asavchenkova01-oss <asavchenkova01@gmail.com>
2025-12-29 14:44:25 +0400

    created folder

---
+++ HW1/main.js
@@ -0,0 +1,26 @@
+// Task 1
+
+let a = 12
+let b =2
+let c = a + b
+
+console.log(c)
+console.log(a*b)
+console.log(a-c)
+console.log(b+c)
+console.log(c/b)
+
+// Task 2 
+
+let x = 20
+let y = -5
+
+console.log(x==y)
+
+// Task 3
+
+let myName = "Anastasia"
+console.log(typeof myName)
+
+let myAge = 24
+console.log(typeof myAge)
